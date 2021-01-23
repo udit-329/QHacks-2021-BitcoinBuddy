544f405c311ee010c05e5d6157617f774cd93d69
KnlnKS <soulkks@hotmail.ca>
2021-01-23 02:10:05 -0500

    made output json

--- services/Reddit/reddit.py
+++ services/Reddit/reddit.py
@@ -1,39 +1,39 @@
 import praw
 import pandas as pd
 import datetime
+import json
 
 red = praw.Reddit(client_id='qm1XcqK5n4MNPQ', client_secret='bYTJwaH9ygGnNyOOPGB8QuyF5YBc7g', user_agent='crypto')
-#crypto specific
-crypto_currency = red.subreddit('CryptoCurrency')
-altcoins = red.subreddit('altcoins')
-crypto_markets = red.subreddit('CryptoMarkets')
-binance = red.subreddit('binance')
-altcointrader = red.subreddit('AltcoinTrader')
 
-def get_data(sub):
-    #Data for 2-3 days
-    hot = sub.hot(limit=1000)
-    #Very recent data
-    rising = sub.rising(limit=100)
-    #Data for a week
-    top = sub.top("week", limit=10000)
 
-    return hot, rising, top
+def get_data(name):
+    sub = red.subreddit(name)
+    top = sub.top("week", limit=1000)
+    return top
 
-def unix_time_convert(time):
+
+def unix_time(time):
     timestamp = datetime.datetime.fromtimestamp(time)
     return timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
-def create_table(sub):
-    hot, rising, top = get_data(sub)
-    hot_all = []
-    
-    for post in hot:
-        print(post.title)
-        hot_all.append([post.title, post.url, post.score, post.selftext, unix_time_convert(post.created)])
-    
-    hot_all = pd.DataFrame(hot_all, columns = ['title', 'url', 'score', 'subreddit', 'num_comments', 'body', 'created'])
-    return hot_all
+
+def create_json(name):
+
+    top = get_data(name)
+    top_all = {'posts': [],
+               'sentiment': []}
+
+    for post in top:
+        print(post)
+        top_all['posts'].append({'title': post.title,
+                                 'url': post.url,
+                                 'score': post.score,
+                                 'body': post.selftext,
+                                 'created_at': unix_time(post.created)})
+
+    with open(str(name) + '.json', 'w') as outfile:
+        json.dump(top_all, outfile)
+
 
 if __name__ == "__main__":
-    create_table(crypto_currency)
+    create_json('bitcoin')
