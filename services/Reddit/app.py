4907b313b2369be7e5080194fc9989194c38b1c4
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-23 11:27:11 +0530

    uhh

--- services/Reddit/app.py
+++ services/Reddit/app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request, redirect, Response
+import reddit
+import fb_class
+import pandas as pd
+import sys
+
+sys.setrecursionlimit(15000)
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bonjour'
+
+firebase_app = fb_class.fire_base_app()
+
+if __name__ == "__main__":
+    data_table = reddit.create_table(reddit.crypto_currency)
+    data_table = data_table.to_json()
+    firebase_app.add_data("test", data_table)
+
--- services/Reddit/reddit.py
+++ services/Reddit/reddit.py
@@ -1,5 +1,7 @@
 import praw
 import fb_class
+import pandas as pd
+import datetime
 
 reddit = praw.Reddit(client_id='qm1XcqK5n4MNPQ', client_secret='bYTJwaH9ygGnNyOOPGB8QuyF5YBc7g', user_agent='crypto')
 
@@ -17,8 +19,28 @@ ethtrader = reddit.subreddit('ethtrader')
 binance = reddit.subreddit('binance')
 
 def get_data(sub):
+    #Data for 2-3 days
     hot = sub.hot(limit=1000)
-    rising = sub.rising(limit=1000)
-    top = sub.top("week", limit=1000)
+    #Very recent data
+    rising = sub.rising(limit=100)
+    #Data for a week
+    top = sub.top("week", limit=10000)
 
     return hot, rising, top
+
+def unix_time_convert(time):
+    timestamp = datetime.datetime.fromtimestamp(time)
+    return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+def create_table(sub):
+    hot, rising, top = get_data(sub)
+    hot_all = []
+    
+    for post in hot:
+        hot_all.append([post.title, post.url, post.score, post.subreddit, post.num_comments, post.selftext, unix_time_convert(post.created)])
+    
+    hot_all = pd.DataFrame(hot_all, columns = ['title', 'url', 'score', 'subreddit', 'num_comments', 'body', 'created'])
+    return hot_all
+
+if __name__ == "__main__":
+    create_table(crypto_currency)
