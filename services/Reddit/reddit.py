cfc8fe1acddb04fa1ab83f7528f2275888037dd0
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-23 09:53:12 +0530

    updated a bit

--- services/Reddit/reddit.py
+++ services/Reddit/reddit.py
@@ -1,21 +1,24 @@
 import praw
+import fb_class
 
 reddit = praw.Reddit(client_id='qm1XcqK5n4MNPQ', client_secret='bYTJwaH9ygGnNyOOPGB8QuyF5YBc7g', user_agent='crypto')
 
-def init_subs():
-    #general
-    algotrading = reddit.subreddit('algotrading')
-    wallStreetBets = reddit.subreddit('wallstreetbets')
-    stocks = reddit.subreddit('stocks')
+#general
+algotrading = reddit.subreddit('algotrading')
+wall_street_bets = reddit.subreddit('wallstreetbets')
+stocks = reddit.subreddit('stocks')
 
-    #crypto specific
-    cryptoCurrency = reddit.subreddit('CryptoCurrency')
-    bitcoin = reddit.subreddit('Bitcoin')
-    btc = reddit.subreddit('btc')
-    cryptoMarkets = reddit.subreddit('CryptoMarkets')
-    ethtrader = reddit.subreddit('ethtrader')
-    binance = reddit.subreddit('binance')
+#crypto specific
+crypto_currency = reddit.subreddit('CryptoCurrency')
+bitcoin = reddit.subreddit('Bitcoin')
+btc = reddit.subreddit('btc')
+crypto_markets = reddit.subreddit('CryptoMarkets')
+ethtrader = reddit.subreddit('ethtrader')
+binance = reddit.subreddit('binance')
 
-test = binance.hot(limit=5)
-for p in test:
-    print(p.title)
+def get_data(sub):
+    hot = sub.hot(limit=1000)
+    rising = sub.rising(limit=1000)
+    top = sub.top("week", limit=1000)
+
+    return hot, rising, top
