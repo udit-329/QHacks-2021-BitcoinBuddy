ee58b6c6ab297ecea94f64fe77f33db223c85d67
KnlnKS <soulkks@hotmail.ca>
2021-01-24 03:01:22 -0500

    great  applie pi in thefiojsku

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import "./App.css";
 
 // Components
@@ -15,10 +16,12 @@ function App() {
       <TopBanner symbol="BTC" />
       <Container>
         <CryptoCard symbol="BTC" />
+        <Row>
+          <PiGraph plat="reddit" coin="bitcoin" />
+          <PiGraph plat="news" coin="bitcoin" />
+        </Row>
         <CryptoColumn symbol="BTC" />
       </Container>
-
-      <PiGraph plat="reddit" coin="bitcoin"/>
     </div>
   );
 }
--- webapp/src/components/PiGraph.js
+++ webapp/src/components/PiGraph.js
@@ -1,27 +1,68 @@
 import React, { useCallback, useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import { PieChart } from "react-minimal-pie-chart";
 import firebase from "../utils/Firebase";
 import "firebase/firestore";
 const firestore = firebase.firestore();
 
 function PiGraph({ plat, coin }) {
-  const [data, setData] = useState({});
+  const [sentData, setSentData] = useState({
+    sentiment: {
+      neg: 10,
+      pos: 25,
+      neu: 12,
+    },
+  });
   const getData = useCallback(() => {
-    console.log(coin);
+    console.log("PiGraph");
     firestore
       .collection(plat)
       .doc(coin)
       .get()
       .then((doc) => {
-        return doc.data();
+        var res = doc.data();
+        res.sentiment.pos *= 1.44;
+        res.sentiment.neg *= 0.69;
+        console.log(res);
+
+        setSentData(res);
       });
   });
 
   useEffect(() => {
-    setData(getData());
-  }, [getData, setData]);
+    getData();
+  }, []);
+  const shiftSize = 7;
   return (
     <div>
-      <div></div>
+      <Card style={{ width: "20rem" }}>
+        <Card.Body>
+          <Card.Title>
+            {plat.charAt(0).toUpperCase() + plat.slice(1) + ":"}
+          </Card.Title>
+          <PieChart
+            lineWidth={30}
+            data={[
+              {
+                title: "Negative",
+                value: sentData.sentiment.neg,
+                color: "#DC3545",
+              },
+              {
+                title: "Positive",
+                value: sentData.sentiment.pos,
+                color: "#28A745",
+              },
+              {
+                title: "Neutral",
+                value: sentData.sentiment.neu,
+                color: "#007BFF",
+              },
+            ]}
+            animate={true}
+          />
+        </Card.Body>
+      </Card>
     </div>
   );
 }
