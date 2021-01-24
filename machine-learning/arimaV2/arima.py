89ef03009b8d4974ba91b1c5a0a000d21354a74b
KnlnKS <soulkks@hotmail.ca>
2021-01-24 07:07:21 -0500

    sadiujfhidosuaf

---
+++ machine-learning/arimaV2/arima.py
@@ -0,0 +1,48 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from pandas.plotting import lag_plot
+from pandas import datetime
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv("./bitcoin_historical_data.csv.csv")
+df.head(5)
+
+plt.figure()
+lag_plot(df['Open'], lag=3)
+plt.title('Predicting the price with ARIMA')
+plt.show()
+
+plt.plot(df["Date"], df["Price"])
+plt.title("TESLA stock price over time")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
+
+train_data, test_data = df[0:int(len(df)*0.7)], df[int(len(df)*0.7):]
+training_data = train_data['Close'].values
+test_data = test_data['Close'].values
+history = [x for x in training_data]
+model_predictions = []
+N_test_observations = len(test_data)
+for time_point in range(N_test_observations):
+    model = ARIMA(history, order=(4,1,0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    model_predictions.append(yhat)
+    true_test_value = test_data[time_point]
+    history.append(true_test_value)
+MSE_error = mean_squared_error(test_data, model_predictions)
+print('Testing Mean Squared Error is {}'.format(MSE_error))
+
+test_set_range = df[int(len(df)*0.7):].index
+plt.plot(test_set_range, model_predictions, color='blue', marker='o', linestyle='dashed',label='Predicted Price')
+plt.plot(test_set_range, test_data, color='red', label='Actual Price')
+plt.title('Prices Prediction')
+plt.xlabel('Date')
+plt.ylabel('Prices')
+plt.legend()
+plt.show()
+
